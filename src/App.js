14b2a929279cef2a97459bd226cfb317076a0030
Mithun-MP Dev <162953538+MITHUNMP157@users.noreply.github.com>
2025-10-10 10:12:50 +0530

    Inserting app component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/studentdb" element={<StudentDetails />} />
       </Routes>
+<Route path="/studentdb" element={<StudentDetails />} />
+      </Routes>
+<Route path="/studentdb" element={<StudentDetails />} />
+      </Routes>
     </div>
   );
 }
